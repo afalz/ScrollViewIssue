4d4aec395a5136ccab10c14c4039d8f8291256d3
unknown-user <unknown-user@10.0.0.31>
2014-11-07 19:40:59 +1100

    Initial check-in of module ScrollViewIssue

---
+++ ScrollViewIssue/MainPage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace ScrollViewIssue
+{
+	public class MainPage : ContentPage
+	{
+		public MainPage ()
+		{
+			ScrollView scrollView = new ScrollView () {
+				HorizontalOptions = LayoutOptions.FillAndExpand,
+				VerticalOptions = LayoutOptions.FillAndExpand,
+				Padding = new Thickness (20),
+				Content = new BoxView () {
+					BackgroundColor = Color.Red,
+					HeightRequest = 400,
+					HorizontalOptions = LayoutOptions.FillAndExpand
+				}
+			};
+
+			Content = scrollView;
+		}
+	}
+}
+
